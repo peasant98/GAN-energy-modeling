c9b6b298e317e9a3b31b9a9a8991e5957e3f5689
Matthew Strong <mast4878@colorado.edu>
2021-04-17 10:54:03 -0600

    add move shell script

---
+++ move.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cp 10/1_csv/1_og_gan_trainsize10.csv GAN-energy-modeling/1_csv/
+cp 20/1_csv/1_og_gan_trainsize20.csv GAN-energy-modeling/1_csv/
+cp 30/1_csv/1_og_gan_trainsize30.csv GAN-energy-modeling/1_csv/
+cp 40/1_csv/1_og_gan_trainsize40.csv GAN-energy-modeling/1_csv/
+cp 50/1_csv/1_og_gan_trainsize50.csv GAN-energy-modeling/1_csv/
+cp 60/1_csv/1_og_gan_trainsize60.csv GAN-energy-modeling/1_csv/
+cp 70/1_csv/1_og_gan_trainsize70.csv GAN-energy-modeling/1_csv/
+cp 80/1_csv/1_og_gan_trainsize80.csv GAN-energy-modeling/1_csv/
+cp 90/1_csv/1_og_gan_trainsize90.csv GAN-energy-modeling/1_csv/
+cp 100/1_csv/1_og_gan_trainsize100.csv GAN-energy-modeling/1_csv/
+
+cp 10/2_csv/1_og_gan_trainsize10.csv GAN-energy-modeling/2_csv/
+cp 20/2_csv/1_og_gan_trainsize20.csv GAN-energy-modeling/2_csv/
+cp 30/2_csv/1_og_gan_trainsize30.csv GAN-energy-modeling/2_csv/
+cp 40/2_csv/1_og_gan_trainsize40.csv GAN-energy-modeling/2_csv/
+cp 50/2_csv/1_og_gan_trainsize50.csv GAN-energy-modeling/2_csv/
+cp 60/2_csv/1_og_gan_trainsize60.csv GAN-energy-modeling/2_csv/
+cp 70/2_csv/1_og_gan_trainsize70.csv GAN-energy-modeling/2_csv/
+cp 80/2_csv/1_og_gan_trainsize80.csv GAN-energy-modeling/2_csv/
+cp 90/2_csv/1_og_gan_trainsize90.csv GAN-energy-modeling/2_csv/
+cp 100/2_csv/1_og_gan_trainsize100.csv GAN-energy-modeling/2_csv/
+
+cp 10/3_csv/1_og_gan_trainsize10.csv GAN-energy-modeling/3_csv/
+cp 20/3_csv/1_og_gan_trainsize20.csv GAN-energy-modeling/3_csv/
+cp 30/3_csv/1_og_gan_trainsize30.csv GAN-energy-modeling/3_csv/
+cp 40/3_csv/1_og_gan_trainsize40.csv GAN-energy-modeling/3_csv/
+cp 50/3_csv/1_og_gan_trainsize50.csv GAN-energy-modeling/3_csv/
+cp 60/3_csv/1_og_gan_trainsize60.csv GAN-energy-modeling/3_csv/
+cp 70/3_csv/1_og_gan_trainsize70.csv GAN-energy-modeling/3_csv/
+cp 80/3_csv/1_og_gan_trainsize80.csv GAN-energy-modeling/3_csv/
+cp 90/3_csv/1_og_gan_trainsize90.csv GAN-energy-modeling/3_csv/
+cp 100/3_csv/1_og_gan_trainsize100.csv GAN-energy-modeling/3_csv/
+
+cp 10/4_csv/1_og_gan_trainsize10.csv GAN-energy-modeling/4_csv/
+cp 20/4_csv/1_og_gan_trainsize20.csv GAN-energy-modeling/4_csv/
+cp 30/4_csv/1_og_gan_trainsize30.csv GAN-energy-modeling/4_csv/
+cp 40/4_csv/1_og_gan_trainsize40.csv GAN-energy-modeling/4_csv/
+cp 50/4_csv/1_og_gan_trainsize50.csv GAN-energy-modeling/4_csv/
+cp 60/4_csv/1_og_gan_trainsize60.csv GAN-energy-modeling/4_csv/
+cp 70/4_csv/1_og_gan_trainsize70.csv GAN-energy-modeling/4_csv/
+cp 80/4_csv/1_og_gan_trainsize80.csv GAN-energy-modeling/4_csv/
+cp 90/4_csv/1_og_gan_trainsize90.csv GAN-energy-modeling/4_csv/
+cp 100/4_csv/1_og_gan_trainsize100.csv GAN-energy-modeling/4_csv/
+
+cp 10/5_csv/1_og_gan_trainsize10.csv GAN-energy-modeling/5_csv/
+cp 20/5_csv/1_og_gan_trainsize20.csv GAN-energy-modeling/5_csv/
+cp 30/5_csv/1_og_gan_trainsize30.csv GAN-energy-modeling/5_csv/
+cp 40/5_csv/1_og_gan_trainsize40.csv GAN-energy-modeling/5_csv/
+cp 50/5_csv/1_og_gan_trainsize50.csv GAN-energy-modeling/5_csv/
+cp 60/5_csv/1_og_gan_trainsize60.csv GAN-energy-modeling/5_csv/
+cp 70/5_csv/1_og_gan_trainsize70.csv GAN-energy-modeling/5_csv/
+cp 80/5_csv/1_og_gan_trainsize80.csv GAN-energy-modeling/5_csv/
+cp 90/5_csv/1_og_gan_trainsize90.csv GAN-energy-modeling/5_csv/
+cp 100/5_csv/1_og_gan_trainsize100.csv GAN-energy-modeling/5_csv/
