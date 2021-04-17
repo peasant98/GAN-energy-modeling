936a611702833cea0b52e66c6d6d898bc0fffff6
Matthew Strong <mast4878@colorado.edu>
2021-04-17 11:05:04 -0600

    move cgan csvs sh scrip

---
+++ move_cgan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp cgan/ts30/1_csv/1_cgan_trainsize30.csv GAN-energy-modeling/1_csv/
+cp cgan/ts100/1_csv/1_cgan_trainsize100.csv GAN-energy-modeling/1_csv/
+
+cp cgan/ts30/2_csv/1_cgan_trainsize30.csv GAN-energy-modeling/2_csv/
+cp cgan/ts100/2_csv/1_cgan_trainsize100.csv GAN-energy-modeling/2_csv/
+
+cp cgan/ts30/3_csv/1_cgan_trainsize30.csv GAN-energy-modeling/3_csv/
+cp cgan/ts100/3_csv/1_cgan_trainsize100.csv GAN-energy-modeling/3_csv/
+
+cp cgan/ts30/4_csv/1_cgan_trainsize30.csv GAN-energy-modeling/4_csv/
+cp cgan/ts100/4_csv/1_cgan_trainsize100.csv GAN-energy-modeling/4_csv/
+
+cp cgan/ts30/5_csv/1_cgan_trainsize30.csv GAN-energy-modeling/5_csv/
+cp cgan/ts100/5_csv/1_cgan_trainsize100.csv GAN-energy-modeling/5_csv/
+
