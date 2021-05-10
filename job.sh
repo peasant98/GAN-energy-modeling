d176492c0460237319eeebf50c59c69b4f2f4a8d
Matthew Strong <mast4878@colorado.edu>
2021-05-10 09:50:26 -0600

    add job

---
+++ job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=6
+#SBATCH --time=03-23
+#SBATCH --partition=shas      
+#SBATCH --qos=long
+#SBATCH --output=sample-%j.out
+
+module load python/3.6.5
+
+python main_acgan.py
