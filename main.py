2f066869c337fe1a6ac2fb724d79ea6776bb02d8
Matt Strong <mast4878@colorado.edu>
2021-06-10 23:39:02 -0600

    another mistake

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import r2_summary_gan as r2
 import shutil
 import os
 
-types = [7, 12, 14, 15, 9, 5]
-gens = [400, 4500, 800, 400, 200, 250]
+types = [9, 5]
+gens = [200, 250]
 if not os.path.isdir('./h5'):
     os.mkdir('./h5')
 if not os.path.isdir('./results'):
     os.mkdir('./results')
 
 for i in range(1,11):
-    num_train = [i*10, i*10, i*10, i*10, i*10, i*10]
+    num_train = [i*10, i*10]
     gan.main(types, gens, num_train)
     print('complete_'+str(i)+'_gan_training')
     
